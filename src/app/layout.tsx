24601e2281f7c53c2a363f233b2ae2c144a27a13
Gabriel Desidério <gabrieldesd7@gmail.com>
2023-08-22 11:44:35 -0300

    fix mobile layout padding top

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
         <Providers>
           <div className="min-h-screen dark:bg-zinc-900 lg:grid lg:grid-cols-app">
             <Sidebar />
-            <main className="max-w-screen px-4 pb-12 pt-8 lg:col-start-2 lg:px-8 lg:pt-8">
+            <main className="max-w-screen px-4 pb-12 pt-24 lg:col-start-2 lg:px-8 lg:pt-8">
               {children}
             </main>
           </div>
